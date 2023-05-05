699ddee4c0ec57eba017f76dab01a20a22a42704
dkkiooko <kithekadankioko@gmail.com>
2023-05-05 13:57:01 +0300

    safeguarded against null values in the formed column

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- ranking glam-rock bands by longetivity
 
-SELECT band_name, IFNULL(split, 2023) - formed AS lifespan
+SELECT band_name, IFNULL(split, 2023) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
