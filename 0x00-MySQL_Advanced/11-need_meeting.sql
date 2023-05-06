748f9fd5a70b3f76bfb0b0158bd8c84e52b7f125
dkkiooko <kithekadankioko@gmail.com>
2023-05-06 12:44:37 +0300

    advanced sql tasks including views, functions, delimeters, procedures and triggers

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,10 @@
+-- creates a view need_meeting
+-- with the following requirements
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS 
+SELECT name FROM students 
+WHERE 
+    score < 80 AND 
+    (last_meeting IS NULL 
+        OR 
+    last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
