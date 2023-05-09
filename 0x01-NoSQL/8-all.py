718c2c4012c74a0abe7f35e48ef8d90c280ef7d9
dkkiooko <kithekadankioko@gmail.com>
2023-05-09 12:13:37 +0300

    made changes to the cursor object. turned it into a list

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 """module that lists all documents in a collection"""
-from pymongo import MongoClient
 
-
-client = MongoClient()
 
 def list_all(mongo_collection):
     """ lists all documents in a collection 
     arg: mongo_collectio -> pymongo collection object"""
-    result = mongo_collection.find()
+    result = list(mongo_collection.find())
 
-    if result.count() == 0:
+    if len(result) == 0:
         return []
     return result
